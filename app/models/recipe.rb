8f1a85e947031f1259aea357e2b8b918038468c4
Rehyan Rhoden <rehyan92@hotmail.co.uk>
2024-09-30 11:05:08 +0100

    added Models Validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,8 @@
 class Recipe < ApplicationRecord
+  has_many :bookmarks
+
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :rating, inclusion: { in: 0..10 }
+  # validates :rating, numericality: { greatwe_than_or_equal_to: 0, less_than_or_equal_to: 10 }
 end
