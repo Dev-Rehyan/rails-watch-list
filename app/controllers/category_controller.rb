a92875a6d09298cebcc244cea8cc9e3cab489dcc
Rehyan Rhoden <rehyan92@hotmail.co.uk>
2024-09-30 14:56:40 +0100

    added Routing, Controller & Views for Categories

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -7,3 +7,26 @@ class CategoryController < ApplicationController
     @category = Category.find(params[:id])
   end
 end
+
+def new
+  @category = Category.new
+end
+
+def create
+  @category = Category.new(category_params)
+  if @category.save
+    redirect_to categories_path
+  else
+    render :new, status: :unprocessable_entity
+  end
+end
+
+def create
+  raise
+end
+
+private
+
+def category_params
+  params.require(:category).permit(:name)
+end
